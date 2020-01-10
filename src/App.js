d8e3dabb183f52d5145bddbb497507c0defe53d9
Rafael <rafaelmf1801@gmail.com>
2020-01-09 22:33:14 -0300

    Add Antd to project and created initial structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Layout } from 'antd';
+import './App.css';
+import isportisticsLogo from './img/logo.png';
+import MainPage from './view/MainPage';
 
-const App = () => <div className="App">Hello</div>;
+const { Header, Content, Footer } = Layout;
+
+const App = () => (
+  <div className="app-container">
+    <Layout className="layout">
+      <Header>
+        <img
+          className="img-logo"
+          src={isportisticsLogo}
+          alt="isportisticsLogo"
+        />
+      </Header>
+      <Content className="antd-content">
+        <MainPage />
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        isportistics challenge ©2020 Created by Rafael Freitas
+      </Footer>
+    </Layout>
+  </div>
+);
 
 export default App;
---
+++ src/view/MainPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <div
+        style={{ background: '#fff', padding: 24, minHeight: 280 }}
+      />
+    );
+  }
+}
